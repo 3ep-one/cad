dc96ef1178ac21d5e72a368fb30d04d37d447ba6
sepehr rafiei <sepehr.r201400@gmail.com>
2020-01-01 20:17:15 +0330

    feat(toplevel): fix bug

--- toplevel/controller.v
+++ toplevel/controller.v
@@ -7,35 +7,39 @@ module controller(
     output_z_stable,
     clk, rst_n);
     input clk, rst_n;
-    input  [63:0] input_a;
-    input  [63:0] input_b;
+    input  [31:0] input_a;
+    input  [31:0] input_b;
     input [2:0] input_op;
     input input_a_stable, input_b_stable, input_op_stable;
     input input_z_ack;
 
-    output output_a_ack, output_b_ack, output_op_ack;
-    output [63:0] output_z;
+    output output_a_ack, output_b_ack;
+	output reg output_op_ack;
+    output [31:0] output_z;
     output output_z_stable;
 
-    reg [63:0]reg_a_stable, [63:0]reg_b_stable;
+    reg [31:0]reg_a_stable;
+	 reg [31:0]reg_b_stable;
     reg [2:0] reg_op_stable;
     parameter IDLE = 3'b000, 
               IF_1 = 3'b001, IF_2 = 3'b010, IF_3 = 3'b011,
-              ID_1 = 3'b100
+              ID_1 = 3'b100,
               EX_1 = 3'b101;
-    reg[1:0] state, next_state;
+    reg[2:0] state, next_state;
 
 
 
     // next state
-    always @(posedge clk or negedge rst_n)
+    always @(posedge clk or posedge rst_n)
     begin
-        if(~rst_n)
-            state <= 2'b00;
-            reg_a_stable <= 64'b0;
-            reg_b_stable <= 64'b0;
-            reg_op_stable <= 3'b0;
-            output_z <= 64'b0;
+        if(rst_n)
+		  begin
+            state <= 3'b000;
+            //reg_a_stable <= 64'b0;
+            //reg_b_stable <= 64'b0;
+            //reg_op_stable <= 3'b0;
+            //output_z <= 64'b0;
+		  end
         else 
             state <= next_state; 
     end
@@ -62,7 +66,7 @@ module controller(
             end
             ID_1:
             begin
-                nex_state = EX_1
+                next_state = EX_1;
             end
             EX_1:
             begin
@@ -77,39 +81,41 @@ module controller(
 
 
 
-    always @(posedge clk or negedge rst_n)
-    begin
-        if(~rst_n)
-            state <= 2'b00;
-            input_a_stable <= 64'b0;
-            input_b_stable <= 64'b0;
-            input_op_stable <= 3'b0;
-        else 
-            state <= next_state; 
-    end
-
+	adder ADDER (reg_a_stable,
+	  reg_b_stable,
+	  input_a_stable,
+	  input_b_stable,
+	  input_z_ack,
+	  clk,
+	  rst_n,
+	  output_z,
+	  output_z_stable,
+	  output_a_ack,
+	  output_b_ack);
+ 
+	 
     // SIGNAL
-    always @(*)
+    always @(state)
     begin
-        output_a_ack <= 1'b0;
-        output_b_ack <= 1'b0;
-        output_op_ack <= 1'b0;
-        output_z_stable <= 1'b0;
-        output_z <= 64'b0;
+        //output_a_ack <= 1'b0;
+        //output_b_ack <= 1'b0;
+        //output_op_ack <= 1'b0;
+        //output_z_stable <= 1'b0;
+        //output_z <= 64'b0;
         case(state)
             IDLE: 
             begin
-                output_z <= 64'b0;
+                //output_z <= 64'b0;
             end
             IF_1: 
             begin
                 reg_a_stable <= input_a;
-                output_a_ack <= 1'b1;
+                //output_a_ack <= 1'b1;
             end
             IF_2: 
             begin
                 reg_b_stable <= input_b;
-                output_b_ack <= 1'b1;
+                //output_b_ack <= 1'b1;
             end
             IF_3:
             begin
@@ -118,33 +124,21 @@ module controller(
             end
             ID_1:
             begin
-                //TODO: ouotput_z <= 
-                if(reg_op_stable == 3'b000)
-                begin
-                    adder ADDER(
-                    input_a,
-                    input_b,
-                    reg_a_stable,
-                    reg_b_stable,
-                    input_z_ack,
-                    clk,
-                    rst_n,
-                    output_z,
-                    output_z_stable,
-                    output_a_ack,
-                    output_b_ack);
-                end
+                ///if(reg_op_stable == 3'b000)
+                //begin
+					
+                //end
 
             end
             EX_1:
             begin
-                output_z_stable <= 1'b1;
+                //output_z_stable <= 1'b1;
             end
             default:
             begin
             //TODO: ?????
-                reg_a_stable <= 64'b0;
-                reg_b_stable <= 64'b0;
+                reg_a_stable <= 32'b0;
+                reg_b_stable <= 32'b0;
                 reg_op_stable <= 3'b0;
             end
         endcase
